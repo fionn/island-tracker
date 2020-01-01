0c854f8b0f6dd7eb43505671fae70b7fc6c75bb5
Fionn Fitzmaurice <fionn@github.com>
2020-01-02 02:10:27 +0800

    Add composition text and freeze dependency
    * Freeze dependency at current commit
    * Replace placeholder text with island name and coordinates
    * Add island URL
    * Don't attach an image (the URL has a preview)
    * Don't assume tweet.place is not None (because it probably is)

--- src/island_tracker.py
+++ src/island_tracker.py
@@ -36,24 +36,19 @@ class Twitter:
     @staticmethod
     def _compose(island: amti.Island) -> dict:
         """Compose a status dictionary compatible with api.status_update"""
-        text = "yolo"
+        name = island.names["Taiwan"]
+        text = f"{name} ({island.geo.lat:.4f}, {island.geo.long:.4f}). {island.url}"
         return {"status": text, "lat": island.geo.lat, "long": island.geo.long}
 
     def update(self, island: amti.Island, dry_run: bool = False) -> tweepy.Status:
         """Post tweet for shape"""
         composition = self._compose(island)
-        LOG.info("Selecting %s", island.name["Taiwan"])
+        LOG.info("Selecting %s", island.names["Taiwan"])
 
         if dry_run:
             return tweepy.Status
 
-        with island.img_file.open("rb") as img_fd:
-            media = self.api.media_upload(filename=island.img_file.name,
-                                          file=img_fd)
-
-        # Not implemented yet, wait for > v3.8.0
-        #self.api.create_media_metadata(media.media_id, f"{shape.caption.en}")
-        return self.api.update_status(**composition, media_ids=[media.media_id])
+        return self.api.update_status(**composition)
 
 def main() -> None:
     """Entry point"""
@@ -69,7 +64,7 @@ def main() -> None:
     tweet = twitter.update(island, args.dry_run)
 
     try:
-        LOG.info("\"%s\" from %s", tweet.text, tweet.place.full_name)
+        LOG.info("\"%s\" at %s", tweet.text, tweet.place)
     except AttributeError:
         if not args.dry_run:
             raise
